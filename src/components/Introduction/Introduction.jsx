c778f4df6a9753ba15ba43ef1835d7b1616ce644
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-10-07 13:28:50 +0600

    feat: Add How it Works Introduction component
    - Added an Introduction  component describing how the platform works.
    - Included icons and text to explain the steps involved in the wedding planning process.

---
+++ src/components/Introduction/Introduction.jsx
@@ -0,0 +1,56 @@
+import { FaBeer, FaClone, FaEye, FaSearch } from "react-icons/fa";
+
+const Introduction = () => {
+  return (
+    <div>
+      <div className=" mt-20">
+        <h1 className=" text-center text-3xl font-semibold">How it Works</h1>
+        <p className=" text-center text-base font-normal text-[#6f6d72] p-5 lg:px-44">
+          Unlock your wedding date with Shaadiyari, your trusted companion at
+          every stage of wedding planning. From selecting the perfect venue and
+          finding your dream bridal attire to choosing the right photographer
+          and planning your dream honeymoon, we bring it all to your fingertips.
+        </p>
+        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-4">
+          <div className=" text-center">
+            <FaSearch className=" mx-auto border text-5xl font-extrabold mb-4 text-[#FF444A] p-2 "></FaSearch>
+            <h1 className=" text-2xl font-normal mb-4">Search Vendor</h1>
+            <p className=" text-[#6f6d72] text-xs ">
+              You can be anywhere in the world, but choosing the vendor at the
+              place you want to be wedded at isn't an issue with our platform.
+            </p>
+          </div>
+          <div className=" text-center">
+            <FaClone className=" mx-auto border text-5xl font-extrabold mb-4 text-[#FF444A] p-2 "></FaClone>
+            <h1 className=" text-2xl font-normal mb-4">Compare</h1>
+            <p className=" text-[#6f6d72] text-xs block ">
+              Exploring your options is a breeze. No matter where you are in the
+              world, selecting the perfect vendor for your dream wedding
+            </p>
+          </div>
+          <div className=" text-center">
+            <FaEye className=" mx-auto border text-5xl font-extrabold mb-4 text-[#FF444A] p-2 "></FaEye>
+            <h1 className=" text-2xl font-normal mb-4">Visiting & Booking</h1>
+            <p className=" text-[#6f6d72] text-xs ">
+              Regardless of your location, our platform makes it effortless to
+              choose and book the ideal vendor for your dream wedding
+              destination.
+            </p>
+          </div>
+          <div className=" text-center">
+            <FaBeer className=" mx-auto border text-5xl font-extrabold mb-4 text-[#FF444A] p-2 "></FaBeer>
+            <h1 className=" text-2xl font-normal mb-4">
+              Celebrate Wedding Day
+            </h1>
+            <p className=" text-[#6f6d72] text-xs ">
+              Your dream wedding is just a click away. Wherever you are, our
+              platform ensures that finding the right vendor for your dream
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/Header/Banner/banner";
+import Introduction from "../../components/Introduction/Introduction";
 
 const Home = () => {
   return (
     <div>
       <div>
         <Banner></Banner>
+        <Introduction></Introduction>
       </div>
     </div>
   );
